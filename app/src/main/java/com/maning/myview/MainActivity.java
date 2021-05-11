07cbc8c9fb25adbbb8894ff738ae9b2d8d8d5430
maning <154292322@qq.com>
2021-05-11 11:30:15 +0800

    升级AndroidX

--- app/src/main/java/com/maning/myview/MainActivity.java
+++ app/src/main/java/com/maning/myview/MainActivity.java
@@ -1,7 +1,7 @@
 package com.maning.myview;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
